bf7a7295eb8b38e14cdd53051ad002ca22e0ef7c
Vladislav <vladik.morozov.2012@mail.ru>
2019-04-03 21:10:56 +1000

    Basic functions

---
+++ WpfdDiary/DayTask.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace DayTasks
+{
+    [DataContract]
+    sealed internal class DayTask
+    {
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public bool Сompleted { get; set; }
+        [DataMember]
+        public string Info { get; set; }
+    }
+
+    sealed internal class TaskList
+    {
+        public List<DayTask> tasks { get; set;}
+
+        public void SaveTaskList(string fileName)
+        {
+            var jsonFormatter = new DataContractJsonSerializer(typeof(List<DayTask>));
+
+            using (var fs = new FileStream(fileName, FileMode.Create))
+            {
+                jsonFormatter.WriteObject(fs, tasks);
+            }
+        }
+
+        public void LoadTaskList(string fileName)
+        {
+            var jsonFormatter = new DataContractJsonSerializer(typeof(List<DayTask>));
+
+            using (var fs = new FileStream(fileName, FileMode.OpenOrCreate))
+            {
+                try
+                {
+                    tasks = (List<DayTask>)jsonFormatter.ReadObject(fs);
+                }
+                catch
+                {
+                    tasks = new List<DayTask>();
+                }
+                
+            }
+        }
+
+        public static string DateToJsonFileName(System.DateTime dataTime) => $"{ dataTime.Year}_{ dataTime.Month}_{ dataTime.Day}.json";
+    }
+}
--- WpfdDiary/MainWindow.xaml.cs
+++ WpfdDiary/MainWindow.xaml.cs
@@ -1,28 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Xml;
+using DayTasks;
 
 namespace WpfdDiary
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
+
+
+    
+
     public partial class MainWindow : Window
     {
+        private static class CalendarInfo
+        {
+            static public  System.DateTime currentDate;
+            static public TaskList taskList = new TaskList();
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            CalendarInfo.currentDate = calendar.SelectedDate ?? calendar.DisplayDate;
+            CalendarInfo.taskList.LoadTaskList(TaskList.DateToJsonFileName(CalendarInfo.currentDate));
+            tasksGrid.ItemsSource = CalendarInfo.taskList.tasks;
+
+            calendar.SelectedDatesChanged += DatesChanged;
+        }
+
+        private void DatesChanged(object sender, RoutedEventArgs e)
+        {
+            CalendarInfo.taskList.SaveTaskList(TaskList.DateToJsonFileName(CalendarInfo.currentDate));
+            var date = (System.Windows.Controls.Calendar) sender;
+            CalendarInfo.taskList.LoadTaskList(TaskList.DateToJsonFileName((System.DateTime) date.SelectedDate));
+            tasksGrid.ItemsSource = CalendarInfo.taskList.tasks;
+            CalendarInfo.currentDate = (System.DateTime) date.SelectedDate;
         }
     }
+
 }
